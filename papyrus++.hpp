8a9d36d678e08ce9610284661da8359d6f4a11d3
Tschilkroete <tschilkroete@users.noreply.github.com>
2016-11-19 17:42:02 +0100

    Renamed *.h to *.hpp

--- papyrus++.hpp
+++ papyrus++.hpp
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "Settings.h"
+#include "settings.hpp"
 
 #include "npp\PluginInterface.h"
 #include <windows.h>
