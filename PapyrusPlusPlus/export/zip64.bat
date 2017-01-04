5eadb8191143bdcfb290c06c3173498f1470221c
Tschilkroete <tschilkroete@users.noreply.github.com>
2017-01-04 21:13:20 +0100

    Automatic ZIP creation

---
+++ PapyrusPlusPlus/export/zip64.bat
@@ -0,0 +1,6 @@
+7z a out\Papyrus++_2.1.0_ZIP_x64.zip .\bin64\Papyrus++.dll
+7z a out\Papyrus++_2.1.0_ZIP_x64.zip ..\Papyrus++.xml
+7z a out\Papyrus++_2.1.0_ZIP_x64.zip .\installation.txt
+copy ..\COPYING licence.txt
+7z a out\Papyrus++_2.1.0_ZIP_x64.zip .\licence.txt
+Del licence.txt
