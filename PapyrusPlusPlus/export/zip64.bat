9b2fbccaff3034ea236d5b193cc37420a84535ad
Tschilkroete <tschilkroete@users.noreply.github.com>
2017-02-05 22:12:27 +0100

    2.2.1

--- PapyrusPlusPlus/export/zip64.bat
+++ PapyrusPlusPlus/export/zip64.bat
@@ -1,6 +1,6 @@
-7z a out\Papyrus++_2.2.0_ZIP_x64.zip .\bin64\Papyrus++.dll
-7z a out\Papyrus++_2.2.0_ZIP_x64.zip ..\Papyrus++.xml
-7z a out\Papyrus++_2.2.0_ZIP_x64.zip .\installation.txt
+7z a out\Papyrus++_2.2.1_ZIP_x64.zip .\bin64\Papyrus++.dll
+7z a out\Papyrus++_2.2.1_ZIP_x64.zip ..\Papyrus++.xml
+7z a out\Papyrus++_2.2.1_ZIP_x64.zip .\installation.txt
 copy ..\COPYING licence.txt
-7z a out\Papyrus++_2.2.0_ZIP_x64.zip .\licence.txt
+7z a out\Papyrus++_2.2.1_ZIP_x64.zip .\licence.txt
 Del licence.txt
